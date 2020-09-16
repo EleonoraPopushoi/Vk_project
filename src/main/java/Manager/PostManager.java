6d550fb567b4b83157003757b870e3b6dc5bd7cd
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-09-16 14:52:27 +0300

    PostManager added

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,17 @@
+package Manager;
+
+import Domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, int postCount, String domain, String url, String query, int ownerOnly) {
+        return null;
+    }
+
+
+    public Post[] delete(int ownerId, int postId) {
+        return null;
+
+    }
+}
